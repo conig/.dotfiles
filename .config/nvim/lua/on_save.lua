d6192fff6d36af50108b289674e827e50ecf850d
James Conigrave <james.conigrave@gmail.com>
2024-11-13 12:55:49 +1100

    add on_save.lua

---
+++ .config/nvim/lua/on_save.lua
@@ -0,0 +1,8 @@
+-- format with conform
+
+vim.api.nvim_create_autocmd("BufWritePre", {
+  pattern = "*",
+  callback = function(args)
+    require("conform").format({ bufnr = args.buf })
+  end,
+})
