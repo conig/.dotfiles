d217e541c6a839bf12ea075fa9f358d022048072
James Conigrave <james.conigrave@gmail.com>
2024-12-10 12:54:51 +1100

    add which-key config

---
+++ .config/nvim/lua/plugins/which-key.lua
@@ -0,0 +1,20 @@
+return{
+  {
+  "folke/which-key.nvim",
+  event = "VeryLazy",
+  opts = {
+     triggers = {
+      { "<auto>", mode = { "n", "i", "o", "t", "c" } }, -- Exclude visual and select modes
+    },
+  },
+  keys = {
+    {
+      "<leader>?",
+      function()
+        require("which-key").show({ global = false })
+      end,
+      desc = "Buffer Local Keymaps (which-key)",
+    },
+  },
+}
+}
