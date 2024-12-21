d572021e27c59911dafd56c309a2fbf0ffbd37c9
James Conigrave <james.conigrave@gmail.com>
2024-12-21 14:55:21 +1100

    add harpoon file

---
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -0,0 +1,58 @@
+return {
+  {
+    "ThePrimeagen/harpoon",
+    branch = "harpoon2",
+    dependencies = { "nvim-lua/plenary.nvim" },
+    lazy = true,
+    keys = {
+      {
+        "<C-e>",
+        function()
+          local harpoon = require "harpoon"
+          harpoon.ui:toggle_quick_menu(harpoon:list())
+        end,
+        desc = "Toggle Harpoon quick menu",
+      },
+      {
+        "<leader>aj",
+        function()
+          local harpoon = require "harpoon"
+          harpoon:list():add()
+        end,
+        desc = "Add file to Harpoon list",
+      },
+      {
+        "<leader>fj",
+        function()
+          local harpoon = require "harpoon"
+          local conf = require("telescope.config").values
+
+          local function toggle_telescope(harpoon_files)
+            local file_paths = {}
+            for _, item in ipairs(harpoon_files.items) do
+              table.insert(file_paths, item.value)
+            end
+
+            require("telescope.pickers")
+              .new({}, {
+                prompt_title = "Harpoon",
+                finder = require("telescope.finders").new_table {
+                  results = file_paths,
+                },
+                previewer = conf.file_previewer {},
+                sorter = conf.generic_sorter {},
+              })
+              :find()
+          end
+
+          toggle_telescope(harpoon:list())
+        end,
+        desc = "Open Harpoon in Telescope",
+      },
+    },
+    config = function()
+      local harpoon = require "harpoon"
+      harpoon.setup()
+    end,
+  },
+}
