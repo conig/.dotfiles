2763e45a903b529cfee3f90957d590f75fe20902
conig <james.conigrave@gmail.com>
2025-01-30 16:01:34 +1100

    fix resizing issues after opening pdf

--- .config/nvim/lua/autocommands.lua
+++ .config/nvim/lua/autocommands.lua
@@ -28,13 +28,52 @@ vim.api.nvim_create_autocmd("RecordingLeave", {
   group = vim.api.nvim_create_augroup("NoiceMacroNotificationDismiss", { clear = true }),
 })
 -- open pdf in zathura
-vim.api.nvim_create_autocmd("BufReadPost", {
+vim.api.nvim_create_autocmd("BufEnter", {
     pattern = "*.pdf",
     callback = function()
         local file = vim.fn.expand("%:p")
         vim.cmd("silent !zathura " .. vim.fn.shellescape(file) .. " &")
-        vim.cmd("bdelete!")  -- Close the buffer
+
+        vim.defer_fn(function()
+            if vim.fn.bufexists(file) == 1 then
+                vim.cmd("bdelete!")
+
+                -- Check if NvimTree is open before closing it
+                local tree_is_open = false
+                for _, win in ipairs(vim.api.nvim_list_wins()) do
+                    local bufname = vim.api.nvim_buf_get_name(vim.api.nvim_win_get_buf(win))
+                    if bufname:match("NvimTree") then
+                        tree_is_open = true
+                        break
+                    end
+                end
+
+                -- Only close and reopen if NvimTree was actually open
+                if tree_is_open then
+                    vim.cmd("NvimTreeClose")
+                    vim.cmd("NvimTreeOpen")
+                end
+            end
+        end, 100)  -- Short delay to allow Zathura to launch
     end,
 })
 
+
+--Don't worry about meta-data updates on /mnt/onedrive/
+vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, {
+  pattern = "*",
+  callback = function()
+    local filename = vim.fn.expand("%:p")
+    if filename:match("^/mnt/onedrive/") then
+      -- Ignore external changes for rclone mount
+      vim.opt.autoread = false
+    else
+      -- Default behavior: reload if unchanged
+      vim.opt.autoread = true
+      if vim.bo.modified == false then
+        vim.cmd("checktime")
+      end
+    end
+  end,
+})
 
