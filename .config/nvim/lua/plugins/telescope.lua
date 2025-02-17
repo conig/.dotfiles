cd6c87c8824faf2614e498a5a3232511ea90dfed
conig <james.conigrave@gmail.com>
2025-02-18 10:35:17 +1100

    add refs to telescope

---
+++ .config/nvim/lua/plugins/telescope.lua
@@ -0,0 +1,29 @@
+return {
+	{
+		"nvim-telescope/telescope-bibtex.nvim",
+		dependencies = { "nvim-telescope/telescope.nvim" },
+		keys = {
+			{ "<leader>kr", "<cmd>Telescope bibtex<CR>", desc = "Open Telescope bibtex", mode = "n" },
+			{
+				"<C-\\>",
+				function()
+					local keys = vim.api.nvim_replace_termcodes("<Esc>:Telescope bibtex<CR>", true, false, true)
+					vim.api.nvim_feedkeys(keys, "n", false)
+				end,
+				desc = "Open Telescope bibtex",
+				mode = "i",
+			},
+		},
+		config = function()
+			require("telescope").setup({
+				extensions = {
+					bibtex = {
+						context = true,
+						context_fallback = true,
+					},
+				},
+			})
+			require("telescope").load_extension("bibtex")
+		end,
+	},
+}
