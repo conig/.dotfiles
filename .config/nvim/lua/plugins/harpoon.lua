e45ee298be9f4aabbc3dc765b59fbc0c8efd2ac8
conig <james.conigrave@gmail.com>
2025-04-30 14:55:12 +1000

    make harpoon wait for a buffer before firing

--- .config/nvim/lua/plugins/harpoon.lua
+++ .config/nvim/lua/plugins/harpoon.lua
@@ -1,58 +1,68 @@
 return {
-  {
-    "ThePrimeagen/harpoon",
-    branch = "harpoon2",
-    dependencies = { "nvim-lua/plenary.nvim" },
-    lazy = true,
-    keys = {
-      {
-        "<C-e>",
-        function()
-          local harpoon = require "harpoon"
-          harpoon.ui:toggle_quick_menu(harpoon:list())
-        end,
-        desc = "Toggle Harpoon quick menu",
-      },
-      {
-        "<leader>aj",
-        function()
-          local harpoon = require "harpoon"
-          harpoon:list():add()
-        end,
-        desc = "Add file to Harpoon list",
-      },
-      {
-        "<leader>fj",
-        function()
-          local harpoon = require "harpoon"
-          local conf = require("telescope.config").values
+	{
+		"ThePrimeagen/harpoon",
+		branch = "harpoon2",
+		dependencies = { "nvim-lua/plenary.nvim" },
+		lazy = true,
+		keys = {
+			{
+				"<C-e>",
+				function()
+					local harpoon = require("harpoon")
+					harpoon.ui:toggle_quick_menu(harpoon:list())
+				end,
+				desc = "Toggle Harpoon quick menu",
+			},
+			{
+				"<leader>aj",
+				function()
+					local harpoon = require("harpoon")
+					harpoon:list():add()
+				end,
+				desc = "Add file to Harpoon list",
+			},
+			{
+				"<leader>fj",
+				function()
+					local bufnr = vim.api.nvim_get_current_buf()
 
-          local function toggle_telescope(harpoon_files)
-            local file_paths = {}
-            for _, item in ipairs(harpoon_files.items) do
-              table.insert(file_paths, item.value)
-            end
+					local function open_harpoon_telescope()
+						local harpoon = require("harpoon")
+						local conf = require("telescope.config").values
+						local file_paths = {}
 
-            require("telescope.pickers")
-              .new({}, {
-                prompt_title = "Harpoon",
-                finder = require("telescope.finders").new_table {
-                  results = file_paths,
-                },
-                previewer = conf.file_previewer {},
-                sorter = conf.generic_sorter {},
-              })
-              :find()
-          end
+						for _, item in ipairs(harpoon:list().items) do
+							table.insert(file_paths, item.value)
+						end
 
-          toggle_telescope(harpoon:list())
-        end,
-        desc = "Open Harpoon in Telescope",
-      },
-    },
-    config = function()
-      local harpoon = require "harpoon"
-      harpoon.setup()
-    end,
-  },
+						require("telescope.pickers")
+							.new({}, {
+								prompt_title = "Harpoon",
+								finder = require("telescope.finders").new_table({
+									results = file_paths,
+								}),
+								previewer = conf.file_previewer({}),
+								sorter = conf.generic_sorter({}),
+							})
+							:find()
+					end
+
+					if vim.api.nvim_buf_is_loaded(bufnr) then
+						open_harpoon_telescope()
+					else
+						vim.api.nvim_create_autocmd("BufReadPost", {
+							buffer = bufnr,
+							once = true,
+							callback = open_harpoon_telescope,
+						})
+					end
+				end,
+				desc = "Open Harpoon in Telescope",
+			},
+		},
+		config = function()
+			local harpoon = require("harpoon")
+			harpoon.setup()
+		end,
+	},
 }
